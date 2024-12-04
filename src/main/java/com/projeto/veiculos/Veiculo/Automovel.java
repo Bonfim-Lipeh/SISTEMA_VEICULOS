fa86df824d0bf514c21ccb29cd21ffcc5349be3a
Felipe Bonfim <felipebonfimpontes@gmail.com>
2024-12-04 20:01:55 -0300

    first commit

--- src/main/java/com/projeto/veiculos/Veiculo/Automovel.java
+++ src/main/java/com/projeto/veiculos/Veiculo/Automovel.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="automovel")
+@DiscriminatorValue("automovel")
 public class Automovel extends Veiculo {
 
     private int passagMax;
